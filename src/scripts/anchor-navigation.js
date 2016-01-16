b840c48a0d274f9f6ef10fa837dc96eecc633f14
Florian Pichler <f.pichler@anfe.ma>
2016-01-16 14:43:04 +0100

    Simplified the js
    Well, I had to try rollup, but that should be enough.

--- src/scripts/anchor-navigation.js
+++ src/scripts/anchor-navigation.js
@@ -1,33 +1,49 @@
-import cycle from './cycle-array';
-
 // make css aware of active JS
 document.documentElement.classList.add('js');
 
 // Store all available targets to array
+const win = window;
 const targets = [...document.querySelectorAll('article section[id]')].map(link => link.id);
+const currentIndex = targets.indexOf(win.location.hash.substr(1));
 
-// Force start on first target
-if (!window.location.hash) {
-	window.location.hash = targets[0];
+let index = currentIndex < 0 ? 0 : currentIndex;
+
+if (!win.location.hash) {
+	win.location.hash = targets[index];
 }
 
-window.addEventListener('keydown', event => {
+function cycle(arr, direction) {
+	const length = arr.length;
+	index = index + direction;
+
+	if (index > length - 1) {
+		index = 0;
+	}
+
+	if (index < 0) {
+		index = length - 1;
+	}
+
+	return arr[index];
+}
+
+win.addEventListener('keydown', event => {
 	const { keyCode } = event; // Left = 37, Up = 38, Right = 39, Down = 40
 
 	switch (keyCode) {
 	case 35: // End
-		window.location.hash = targets[targets.length - 1];
+		win.location.hash = targets[targets.length - 1];
 		break;
 	case 36: // Home
-		window.location.hash = targets[0];
+		win.location.hash = targets[0];
 		break;
 	case 37: // Left
 	case 38: // Up
-		window.location.hash = cycle(targets, -1);
+		win.location.hash = cycle(targets, -1);
 		break;
 	case 39: // Right
 	case 40: // Down
-		window.location.hash = cycle(targets, 1);
+		win.location.hash = cycle(targets, 1);
 		break;
 	default:
 		break;

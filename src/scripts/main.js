18db4461d4a76b43646110663d05bd6f923bbec7
Florian Pichler <f.pichler@anfe.ma>
2016-01-17 22:17:40 +0100

    Extended presentation script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,16 @@
-import './anchor-navigation';
+import slides from './slides';
+
+const doc = document;
 
 // make css aware of active JS
-document.documentElement.classList.add('js');
+doc.documentElement.classList.add('js');
+
+//
+slides([...doc.querySelectorAll('.slides section')].map((slide, index) => {
+	const title = slide.querySelector('h1, h2, h3, h4, h5, h6');
+
+	return {
+		title: `${title ? (title.textContent + ' - ') : ''}${index + 1}`,
+		hash: slide.id,
+	};
+}));
---
+++ src/scripts/slides.js
@@ -0,0 +1,92 @@
+const doc = document;
+const win = window;
+
+export class Slides {
+	constructor(slidesArray, options) {
+		this.options = Object.assign({
+			title: `${doc.title}`,
+		}, options);
+
+		this.current = 0;
+		this.slides = slidesArray;
+
+		if (slidesArray.length) {
+			this.go(this.initialIndex);
+			this.attach();
+		}
+	}
+
+	get initialIndex() {
+		if (win.location.hash) {
+			const hash = win.location.hash.substr(1);
+
+			return this.slides.findIndex(slide => {
+				return slide.hash === hash;
+			}) || 0;
+		}
+
+		return 0;
+	}
+
+	go(dest) {
+		const index = (dest < 0) ? (this.slides.length - 1 - dest) : dest;
+		const slide = this.slides[index];
+
+		this.current = index;
+		win.location.hash = slide.hash;
+		doc.title = `${slide.title} - ${this.options.title}`;
+	}
+
+	next() {
+		let n = this.current + 1;
+
+		if (n >= this.slides.length) {
+			n = 0;
+		}
+
+		this.go(n);
+	}
+
+	previous() {
+		let p = this.current - 1;
+
+		if (p < 0) {
+			p = this.slides.length - 1;
+		}
+
+		this.go(p);
+	}
+
+	onKeyDown(event) {
+		const { keyCode } = event; // Left = 37, Up = 38, Right = 39, Down = 40
+
+		switch (keyCode) {
+		case 35: // End
+			this.go(-1);
+			break;
+		case 36: // Home
+			this.go(0);
+			break;
+		case 37: // Left
+		case 38: // Up
+			this.previous();
+			break;
+		case 39: // Right
+		case 40: // Down
+			this.next();
+			break;
+		default:
+			break;
+		}
+	}
+
+	attach() {
+		this._onKeyDownBound = this.onKeyDown.bind(this);
+
+		win.addEventListener('keydown', this._onKeyDownBound);
+	}
+}
+
+export default function slides(selector, options) {
+	return new Slides(selector, options);
+}
